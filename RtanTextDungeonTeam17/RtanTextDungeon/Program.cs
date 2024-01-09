9340969b487751d0c9ef32c40d6c780ae268d86e
ChocoMucho <vkclssha14@gmail.com>
2024-01-09 20:06:00 +0900

    feat : 캐릭터 생성 기능 추가
    이름 입력 받고 캐릭터 생성하는 함수 추가

--- RtanTextDungeonTeam17/RtanTextDungeon/Program.cs
+++ RtanTextDungeonTeam17/RtanTextDungeon/Program.cs
@@ -16,13 +16,13 @@ namespace RtanTextDungeon
             Shop shop = new Shop();
             // 플레이어를 불러오기를 통해 생성한다.
             Console.WriteLine("데이터를 불러오는중...");
-            Player player = new Player(1, "르탄이", Define.PlayerClass.Worrior, 10, 5, 100, 100, 1500);
+            //Player player = new Player(1, "르탄이", Define.PlayerClass.Worrior, 10, 5, 100, 100, 1500);
             Thread.Sleep(1000);
 
             // 인 게임 Dungeon클래스
             Dungeon dungeon = new Dungeon();
             // EnterGame 메서드로 게임에 입장한다. 들고갈 정보는 Player와 Shop
-            dungeon.EnterGame(player, shop);
+            dungeon.EnterGame(shop);
         }
     }
 }
